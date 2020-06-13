a4de638dafaf0eb0ee50cf209d53aa7904b03e13
bnlcas <bnlcas@gmail.com>
2020-06-13 10:50:09 -0700

    enabled configuration on tetacono settings

--- Work/Sandbox/Tetracono/js/tetracono.js
+++ Work/Sandbox/Tetracono/js/tetracono.js
@@ -1,5 +1,9 @@
+//System Parameters:
+//var time_scaling = 2 * Math.PI / 1000; // Munari Setting;
+var time_scaling = 2 * Math.PI / 100; // Faster
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(65, window.innerWidth/window.innerHeight, 0.1,100);
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1,100);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -29,10 +33,10 @@ cones[3].rotation.set(0,0,3*Math.PI/2);
 //Box:
 var boxfaces = [];
 var box_width = 0.08;// 0.076;
-var box_thickness = 0.01;
+var box_thickness = 0.005;
 for(var i = 0; i < 4; i ++)
 {
-  var geometry = new THREE.BoxGeometry(box_thickness,box_width*2, box_width*2);
+  var geometry = new THREE.BoxGeometry(box_thickness,box_thickness + box_width*2, box_thickness + box_width*2);
   var material = new THREE.MeshLambertMaterial();
   var wall = new THREE.Mesh(geometry, material);
   scene.add(wall);
@@ -61,14 +65,13 @@ scene.add(light);
 var rotation_periods = [90, 60, 108, 72];
 function RotateCones(delta_time)
 {
-  scaling = 0.001;
-  cones[0].rotateY(delta_time * scaling / rotation_periods[0]);
-  cones[1].rotateY(delta_time * scaling / rotation_periods[1]);
-  cones[2].rotateY(delta_time * scaling / rotation_periods[2]);
-  cones[3].rotateY(delta_time * scaling / rotation_periods[3]);
+  cones[0].rotateY(delta_time * time_scaling / rotation_periods[0]);
+  cones[1].rotateY(delta_time * time_scaling / rotation_periods[1]);
+  cones[2].rotateY(delta_time * time_scaling / rotation_periods[2]);
+  cones[3].rotateY(delta_time * time_scaling / rotation_periods[3]);
 }
 
-camera.position.set(0,0,0.2);
+camera.position.set(0,0,0.3);
 camera.up = new THREE.Vector3(0,1,0);
 camera.lookAt(new THREE.Vector3(0,0,0));
 
@@ -83,16 +86,31 @@ function animate(currentTime)
 {
   if(currentTime)
   {
-  if(!startingTime){startingTime=currentTime;}
-  if(!lastTime){lastTime=currentTime;}
-  totalElapsedTime=(currentTime-startingTime);
-  elapsedSinceLastLoop=(currentTime-lastTime);
-  lastTime=currentTime;
-  RotateCones(elapsedSinceLastLoop);
+    //In Milliseconds
+    if(!startingTime){startingTime=currentTime;}
+    if(!lastTime){lastTime=currentTime;}
+    totalElapsedTime=(currentTime-startingTime);
+    elapsedSinceLastLoop=(currentTime-lastTime);
+    lastTime=currentTime;
+    RotateCones(elapsedSinceLastLoop);
   }
-
 
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
 animate();
+
+
+//Configuration:
+function UpdateRotationRate(scaling = 10)
+{
+  var default_rate = 2 * Math.PI / 1000;
+  time_scaling = default_rate * scaling;
+}
+
+function UpdateTexture(texture_file)
+{
+  cones.forEach(
+    cone => (cone.material.map = new THREE.TextureLoader().load(texture_file))
+  );
+}
